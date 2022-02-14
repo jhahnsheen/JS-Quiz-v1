58bae78442aa2312d3863e2170174db2b32f6214
jhahnsheen <91440103+jhahnsheen@users.noreply.github.com>
2022-02-14 01:23:30 -0500

    quiz.js updated, index.html and highSchores.html updated

---
+++ assets/js/quiz.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        question: "Which of these variable types is not able to be changed once it has been declared?",
+        choices: ["var", "const", "let", "is"],
+        answer: "const"
+    }, 
+    {
+        question: "All of these data types are considered a primitive value except:",
+        choices: ["array", "boolean", "string", "null"],
+        answer: "array"
+    }, 
+    {
+        question: "If a variable is declared, let quiz = 1, and it is later reassigned, var quiz = 3, the result would be:",
+        choices: ["var quiz = 3", "let quiz = 3", "let quiz = 1", "Syntax error"],
+        answer: "Syntax error"
+    }, 
+    {
+        question: "Strict equality is designated by:",
+        choices: ["=", "==", "===", "All of the above"],
+        answer: "==="
+    }, 
+    {
+        question: "What do we call a set of statements that performs a task or calculates a value?",
+        choices: ["rules", "conditions", "calculations", "function"],
+        answer: "function"
+    }
+];
